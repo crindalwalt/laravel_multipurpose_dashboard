145a6c0a07043593c1e1e302cb497a08b648504a
Shahzad Farooq <crindalwalt@protonmail.com>
2024-01-08 16:52:19 +0500

    admin template integrated into laravel application (SF-02)

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route("dashboard");
 });
 
 Route::get('/dashboard', function () {
@@ -29,3 +29,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+require __DIR__.'/template.php';
